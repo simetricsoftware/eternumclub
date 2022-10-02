0415f9278b497e547706247a42372c3587bdeb73
Christian Albán <christianalbanctdb1d@gmail.com>
2022-10-01 23:55:39 -0500

    Carga de icono

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -2,60 +2,57 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta content="IE=edge" http-equiv="X-UA-Compatible">
-  <meta content="width=device-width,initial-scale=1" name="viewport">
-  <meta content="description" name="eternum club compra NFTs">
-  <meta name="google" content="notranslate" />
-  <meta content="Simetric Software - Saishimura name="author">
+    <meta charset="UTF-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width,initial-scale=1" name="viewport">
+    <meta content="description" name="eternum club compra NFTs">
+    <meta name="google" content="notranslate" />
+    <meta content="Simetric Software - Saishimura" name="author">
 
-  <!-- Disable tap highlight on IE -->
-  <meta name="msapplication-tap-highlight" content="no">
-  
-  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/apple-icon-180x180.png') }}">
-  <link href="{{ asset('assets/favicon.ico') }}" rel="icon">
+    <!-- Disable tap highlight on IE -->
+    <meta name="msapplication-tap-highlight" content="no">
 
-  <title>Eternum Club</title>  
+    <link href="{{ asset('favicon.ico') }}" rel="icon">
 
-<link href="{{ asset('css/main.3f6952e4.css') }}" rel="stylesheet">
+    <title>Eternum Club</title>
 
-<link rel="icon" href="{{ asset('ico.ico') }}">
+    <link href="{{ asset('css/main.3f6952e4.css') }}" rel="stylesheet">
 
 
 </head>
 
 <body class="minimal">
-<div id="site-border-left"></div>
-<div id="site-border-right"></div>
-<div id="site-border-top"></div>
-<div id="site-border-bottom"></div>
-<!-- Add your content of header -->
-<header>
-  <nav class="navbar  navbar-fixed-top navbar-inverse">
-    <div class="container">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
+    <div id="site-border-left"></div>
+    <div id="site-border-right"></div>
+    <div id="site-border-top"></div>
+    <div id="site-border-bottom"></div>
+    <!-- Add your content of header -->
+    <header>
+        <nav class="navbar  navbar-fixed-top navbar-inverse">
+            <div class="container">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-      <div class="collapse navbar-collapse" id="navbar-collapse">
-        <ul class="nav navbar-nav ">
-          <li><a href="{{ route('home') }}" title="">I : Home</a></li>
-          <li><a href="{{ route('events') }}" title="">II : Eventos</a></li>
-          <li><a href="{{ route('club') }}" title="">III : Club</a></li>
-        </ul>
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav ">
+                        <li><a href="{{ route('home') }}" title="">I : Home</a></li>
+                        <li><a href="{{ route('events') }}" title="">II : Eventos</a></li>
+                        <li><a href="{{ route('club') }}" title="">III : Club</a></li>
+                    </ul>
 
 
-      </div> 
-    </div>
-  </nav>
-</header>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-@yield('content')
+    @yield('content')
 
-<script type="text/javascript" src="{{ asset('js/main.70a66962.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/main.70a66962.js') }}"></script>
 </body>
 
 </html>
--- resources/views/web/events.blade.php
+++ resources/views/web/events.blade.php
@@ -3,136 +3,136 @@
 @section('content')
 <div class="section-container">
     <div class="container">
-      <div class="row">
-        
-        <div class="col-sm-8 col-sm-offset-2 section-container-spacer">
-          <div class="text-center">
-            <h1 class="h2">II : Eventos</h1>
-            <p style="font-size: 1.7rem;">Cada una de nuestras colecciones de NFTs incluye artes únicos que equivalen a una entrada a un evento exclusivo o son canjeables por un objeto en la vida real
-            </p>
-          </div>
-        </div>
-  
-        <div class="col-md-12">
-       
-        <div id="myCarousel" class="carousel slide projects-carousel">
-          <!-- Carousel items -->
-          <div class="carousel-inner">
-              <div class="item active">
-                  <div class="row">
-                      <div class="col-sm-4">
-  
-                        <a href="./work.html" title="" class="black-image-project-hover">
-                          <img src="./assets/images/halloween.jpg" alt="" class="img-responsive">
-                        </a>
-                        <div class="card-container card-container-lg">
-                          <h4>001/001</h4>
-                          <h3>Halloween Boiler room 2022</h3>
-                          <p style="font-size: 1.2rem;"> <b>44 nfts para celebrar halloween en el boiler room de DJ Ismael</b></p>
-                          <a href="{{ route('galery') }}" title="" class="btn btn-default">COMPRAR</a>
+        <div class="row">
+
+            <div class="col-sm-8 col-sm-offset-2 section-container-spacer">
+                <div class="text-center">
+                    <h1 class="h2">II : Eventos</h1>
+                    <p style="font-size: 1.7rem;">Cada una de nuestras colecciones de NFTs incluye artes únicos que equivalen a una entrada a un evento exclusivo o son canjeables por un objeto en la vida real
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+
+                <div id="myCarousel" class="carousel slide projects-carousel">
+                    <!-- Carousel items -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <div class="row">
+                                <div class="col-sm-4">
+
+                                    <div class="black-image-project-hover">
+                                        <img src="./assets/images/halloween.jpg" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="card-container card-container-lg">
+                                        <h4>001/001</h4>
+                                        <h3>Halloween Boiler room 2022</h3>
+                                        <p style="font-size: 1.2rem;"> <b>44 nfts para celebrar halloween en el boiler room de DJ Ismael</b></p>
+                                        <a href="{{ route('galery') }}" title="" class="btn btn-default">COMPRAR</a>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="black-image-project-hover">
+                                        <img src="./assets/images/prox-2.jpg" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="card-container card-container-lg">
+                                        <h4>002/001</h4>
+                                        <h3>Próximamente</h3>
+                                        <p style="font-size: 1.2rem;"> <b>Wait for it</b></p>
+                                        <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="black-image-project-hover">
+                                        <img src="./assets/images/prox-3.jpg" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="card-container card-container-lg">
+                                        <h4>003/001</h4>
+                                        <h3>Próximamente</h3>
+                                        <p style="font-size: 1.2rem;"> <b>Wait for it</b></p>
+                                        <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <!--/row-->
                         </div>
-                      </div>
-                      <div class="col-sm-4">
-                        <a href="./work.html" title="" class="black-image-project-hover">
-                          <img src="./assets/images/prox-2.jpg" alt="" class="img-responsive">
-                        </a>
-                        <div class="card-container card-container-lg">
-                          <h4>002/001</h4>
-                          <h3>Próximamente</h3>
-                          <p style="font-size: 1.2rem;"> <b>Wait for it</b></p>
-                          <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
+                        <!--/item-->
+                        <div class="item">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <a href="./work.html" class="black-image-project-hover">
+                                        <img src="./assets/images/prox-4.jpg" alt="Image" class="img-responsive">
+                                    </a>
+                                    <div class="card-container">
+                                        <h4>004/001</h4>
+                                        <h3>Próximamente</h3>
+                                        <p>Wait for it</p>
+                                        <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <a href="./work.html" class="black-image-project-hover">
+                                        <img src="./assets/images/prox-5.jpg" alt="Image" class="img-responsive">
+                                    </a>
+                                    <div class="card-container">
+                                        <h4>005/001</h4>
+                                        <h3>Próximamente</h3>
+                                        <p>Wait for it</p>
+                                        <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <a href="./work.html" class="black-image-project-hover">
+                                        <img src="./assets/images/prox-6.jpg" alt="Image" class="img-responsive">
+                                    </a>
+                                    <div class="card-container">
+                                        <h4>006/001</h4>
+                                        <h3>Próximamente</h3>
+                                        <p>Wait for it</p>
+                                        <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <!--/row-->
                         </div>
-                      </div>
-                      <div class="col-sm-4">
-                        <a href="./work.html" title="" class="black-image-project-hover">
-                          <img src="./assets/images/prox-3.jpg" alt="" class="img-responsive">
-                        </a>
-                        <div class="card-container card-container-lg">
-                          <h4>003/001</h4>
-                          <h3>Próximamente</h3>
-                          <p style="font-size: 1.2rem;"> <b>Wait for it</b></p>
-                          <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
-                        </div>
-                      </div>
-                      
-                  </div>
-                  <!--/row-->
-              </div>
-              <!--/item-->
-              <div class="item">
-                  <div class="row">
-                    <div class="col-sm-4">
-                      <a href="./work.html" class="black-image-project-hover">
-                        <img src="./assets/images/prox-4.jpg" alt="Image" class="img-responsive">
-                      </a>
-                      <div class="card-container">
-                        <h4>004/001</h4>
-                        <h3>Próximamente</h3>
-                        <p>Wait for it</p>
-                        <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
-                      </div>
+
+                        <!--/item-->
                     </div>
-                     <div class="col-sm-4">
-                        <a href="./work.html" class="black-image-project-hover">
-                          <img src="./assets/images/prox-5.jpg" alt="Image" class="img-responsive">
-                        </a>
-                        <div class="card-container">
-                          <h4>005/001</h4>
-                          <h3>Próximamente</h3>
-                          <p>Wait for it</p>
-                          <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
-                        </div>
-                      </div>
-                      
-                      <div class="col-sm-4">
-                        <a href="./work.html" class="black-image-project-hover">
-                          <img src="./assets/images/prox-6.jpg" alt="Image" class="img-responsive">
-                        </a>
-                        <div class="card-container">
-                          <h4>006/001</h4>
-                          <h3>Próximamente</h3>
-                          <p>Wait for it</p>
-                          <a href="./galery.html" title="" class="btn btn-default">COMPRAR</a>
-                        </div>
-                      </div>
-                      
-                  </div>
-                  <!--/row-->
-              </div>
-              
-              <!--/item-->
-          </div>
-          <!--/carousel-inner--> 
-          <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-  
-          <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
-       </div>
-  
-  
-  
-      <!--/myCarousel-->
-      </div>
-  
-  
-  
-      </div>
-    </div>
-  </div>
-  
-  
-  <footer class="footer-container text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <p>ETERNUM CLUB ©  | IS A <a style="text-decoration:none;" href="https://simetricsoftware.com/">SIMETRIC SOFTWARE PRODUCTION</a></p>
+                    <!--/carousel-inner-->
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+                </div>
+
+
+
+                <!--/myCarousel-->
+            </div>
+
+
+
         </div>
-      </div>
     </div>
-  </footer>
-  
-  <script>
-    document.addEventListener("DOMContentLoaded", function (event) {
-       navActivePage();
+</div>
+
+
+<footer class="footer-container text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>ETERNUM CLUB © | IS A <a style="text-decoration:none;" href="https://simetricsoftware.com/">SIMETRIC SOFTWARE PRODUCTION</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        navActivePage();
     });
-  </script>
+</script>
 @endsection
