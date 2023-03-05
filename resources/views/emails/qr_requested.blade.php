36a1488813a168efa9bcd0c3e7c530bb4b500f9e
Christian Albán <chris_alban@live.com>
2023-03-04 19:18:14 -0500

    Plantilla de correo electrónico

--- resources/views/emails/qr_requested.blade.php
+++ resources/views/emails/qr_requested.blade.php
@@ -1,23 +1,49 @@
-<div style="display: flex;flex-direction: column;width: 80%;justify-content: center;align-items: center;margin: 0 auto;">
-    <div class="logo">
-        <img src="{{ asset('assets/images/eternumlogo.png') }}">
-    </div>
-    <h1>
-        {{ $client_name }}
-    </h1>
-    <p> Bienvenido a Eternum club, ahora tu NFT se encuentra almacenado para siempre en la blockchain, a continuación te dejamos el QR que debes presentar el día del evento para poder ingresar
 
-    </p>
-    <div class="cuerpo">
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Invitación a la fiesta egipcia</title>
+</head>
+<body>
+	<table width="100%" cellpadding="0" cellspacing="0" border="0">
+		<tr>
+			<td align="center" valign="top" style="background-color: #f5f5f5;">
+				<table width="600" cellpadding="0" cellspacing="0" border="0">
+					<!-- Image -->
+					<tr>
+						<td align="center" style="padding: 0 0 20px 0;">
+							<img src="{{ asset('assets/images/lachavizav.webp') }}" alt="La Chaviza" width="800" style="max-width: 70%; margin-top: 25px;" />
+						</td>
+					</tr>
+					<!-- QR Code -->
+					<tr>
+						<td align="center" style="padding: 0 0 20px 0;">
+							<img src="{{ $qr_url }}" alt="QR Code" width="200" style="max-width: 33%;" />
+						</td>
+					</tr>
+					<!-- Message -->
+					<tr>
+						<td align="center" style="padding: 0 0 20px 0;">
+							<p style="max-width: 50%; margin: 0 auto;">
+								Gracias por ser parte de la Chavisa no pierdas este código QR con este código y tu invitación podrás ingresar a la fiesta egipcia, pronto a tu WhatsApp se te enviará la ubicación. Gracias por preferirnos 🪩
+							</p>
+						</td>
+					</tr>
+					<!-- Footer -->
+					<tr>
+						<td align="center" style="padding: 20px 0 30px 0;">
+							<img src="{{ asset('assets/images/eternumlogo.png') }}" alt="Eternum Club" width="100" style="display: block;" />
+								<p style="color: #777; margin-top: 20px;">
+                  Este evento fue patrocinado por Eternum Club, si deseas hacer tu evento y utilizar nuestro sistema de seguridad por QR, contactanos al correo <a href="mailto:info@simetricsoftwate.com">info@simetricsoftware.com</a>.
+                  <br>
+                  Eternum Club es un producto de <a href="https://simetricsoftware.com/">Simetric Software</a>.
+								</p>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
 
-        <div class="relleno">
-            <p1>
-                <img width="250px" src="{{ $qr_url }}" alt="">
-            </p1>
-
-        </div>
-
-    </div>
-    <p>Saludos</p>
-    <p>El equipo de Eternum Club</p>
-</div>
